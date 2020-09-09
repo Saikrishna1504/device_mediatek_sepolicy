ceb01d4b178cd78442b8e6d656a435d5a702c1e1
Aayush Gupta <aayushgupta219@gmail.com>
2020-09-09 11:47:36 +0000

    sepolicy: Add a README and makefiles required for inclusion
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I9c25d258ac2611dc3982552fc8b32e94cb7909be

---
+++ SEPolicy.mk
@@ -0,0 +1,9 @@
+# Board specific SELinux policy variable definitions
+BOARD_SEPOLICY_DIRS := \
+        device/mediatek/sepolicy/vendor
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
+        device/mediatek/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
+        device/mediatek/sepolicy/private
